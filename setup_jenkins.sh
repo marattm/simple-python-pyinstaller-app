4d2938f47810298623276df31513cc8adf83da06
Marat Monnié <mtulepbayevmonnie@hawk.iit.edu>
2018-09-01 12:38:41 -0500

    Add initial Jenkinsfile + bash script to start Jenkins on docker.

---
+++ setup_jenkins.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# '''
+# Script for launching Jenkins on docker. 
+# '''
+docker run \
+  --rm \
+  -u root \
+  -p 8080:8080 \
+  --name jenkins-tutorials \
+  -v jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$HOME":/home \
+  jenkinsci/blueocean 
+  
+
+# '''
+# Script for accessing Jenkins on docker, and having a prompt command window. 
+# '''
+# docker exec -it jenkins-tutorials bash
